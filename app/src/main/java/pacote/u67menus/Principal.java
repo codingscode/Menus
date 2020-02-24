7ac6770a3a8af842d6a4d3f3b12c215be80db76e
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-24 19:07:23 -0300

    zero

---
+++ app/src/main/java/pacote/u67menus/Principal.java
@@ -0,0 +1,13 @@
+package pacote.u67menus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
