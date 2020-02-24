7dc3f150292eb62888bec292e3524121d2be871f
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-24 19:59:34 -0300

    parte 1

--- app/src/main/java/pacote/u67menus/Principal.java
+++ app/src/main/java/pacote/u67menus/Principal.java
@@ -2,6 +2,9 @@ package pacote.u67menus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.itemSalvar:
+                Toast.makeText(Principal.this, "Item Salvar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.itemEditar:
+                Toast.makeText(Principal.this, "Item Editar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.itemConfiguracao:
+                Toast.makeText(Principal.this, "Item Configuração", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
